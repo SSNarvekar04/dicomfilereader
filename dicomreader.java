2e634049c0e72f48be5bd804bbf202f6f392af04
SSNarvekar04 <salonisachindranathnarvekar@gmail.com>
2025-06-09 19:09:39 +0530

    Update dicomreader.java

--- dicomreader.java
+++ dicomreader.java
@@ -13,12 +13,18 @@ public class dicomreader {
             loadDicomFile(dicomFile);
 
 
-            System.out.println("Study Instance UID : " + getTagInformation(TagFromName.StudyInstanceUID));
-            System.out.println("Series Instance UID : " + getTagInformation(TagFromName.SeriesInstanceUID));
-
-            System.out.println("Patient Name :"+ getTagInformation(TagFromName.PatientName));
-            System.out.println("Modality: " + getTagInformation(TagFromName.Modality));
-            System.out.println("Study Date: " + getTagInformation(TagFromName.StudyDate));
+           System.out.println("Study Instance UID : " + getTagInformation(TagFromName.StudyInstanceUID));
+           System.out.println("Series Instance UID : " + getTagInformation(TagFromName.SeriesInstanceUID));
+           System.out.println("SOP Instance UID : " + getTagInformation(TagFromName.SOPInstanceUID));
+            
+           System.out.println("Patient Name : " + getTagInformation(TagFromName.PatientName));
+           System.out.println("Patient ID : " + getTagInformation(TagFromName.PatientID));
+           System.out.println("Patient Birth Date : " + getTagInformation(TagFromName.PatientBirthDate));
+           System.out.println("Patient Sex : " + getTagInformation(TagFromName.PatientSex));
+            
+           System.out.println("Modality : " + getTagInformation(TagFromName.Modality));
+           System.out.println("Study Date : " + getTagInformation(TagFromName.StudyDate));
+           System.out.println("Study Time : " + getTagInformation(TagFromName.StudyTime));
 
         } catch (Exception e) {
             e.printStackTrace();

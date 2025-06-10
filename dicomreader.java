870c2ef00e5bcc76b8c8de44bdda61e37c82ddaa
SSNarvekar04 <salonisachindranathnarvekar@gmail.com>
2025-06-10 09:46:55 +0530

    Update dicomreader.java

--- dicomreader.java
+++ dicomreader.java
@@ -10,21 +10,19 @@ public class dicomreader {
     public static void main(String[] args) {
         String dicomFile = "D:\\dicomreader\\dicom.dcm";
         try {
-            loadDicomFile(dicomFile);
+            readDicomFile(dicomFile); 
+            System.out.println("Study Instance UID : " + tag(TagFromName.StudyInstanceUID));
+            System.out.println("Series Instance UID : " + tag(TagFromName.SeriesInstanceUID));
+            System.out.println("SOP Instance UID : " + tag(TagFromName.SOPInstanceUID));
 
+            System.out.println("Patient Name : " + tag(TagFromName.PatientName));
+            System.out.println("Patient ID : " + tag(TagFromName.PatientID));
+            System.out.println("Patient Birth Date : " + tag(TagFromName.PatientBirthDate));
+            System.out.println("Patient Sex : " + tag(TagFromName.PatientSex));
 
-           System.out.println("Study Instance UID : " + tag(TagFromName.StudyInstanceUID));
-           System.out.println("Series Instance UID : " + tag(TagFromName.SeriesInstanceUID));
-           System.out.println("SOP Instance UID : " +tag(TagFromName.SOPInstanceUID));
-            
-           System.out.println("Patient Name : " + tag(TagFromName.PatientName));
-           System.out.println("Patient ID : " +tag(TagFromName.PatientID));
-           System.out.println("Patient Birth Date : " + tag(TagFromName.PatientBirthDate));
-           System.out.println("Patient Sex : " + tag(TagFromName.PatientSex));
-            
-           System.out.println("Modality : " +tag(TagFromName.Modality));
-           System.out.println("Study Date : " + tag(TagFromName.StudyDate));
-           System.out.println("Study Time : " + tagn(TagFromName.StudyTime));
+            System.out.println("Modality : " + tag(TagFromName.Modality));
+            System.out.println("Study Date : " + tag(TagFromName.StudyDate));
+            System.out.println("Study Time : " + tag(TagFromName.StudyTime));
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,7 +33,7 @@ public class dicomreader {
         list.read(dicomFilePath);
     }
 
-    public static String getTagInformation(AttributeTag attrTag) {
+    public static String tag(AttributeTag attrTag) {
         return Attribute.getDelimitedStringValuesOrEmptyString(list, attrTag);
     }
 }

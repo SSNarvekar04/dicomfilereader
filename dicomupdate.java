1b046838921a0e7cd41b8a931c2b5e5ae6b01b91
SSNarvekar04 <salonisachindranathnarvekar@gmail.com>
2025-06-20 15:27:19 +0530

    Add files via upload

--- dicomupdate.java
+++ dicomupdate.java
@@ -1,8 +1,7 @@
-
 import org.dcm4che2.data.DicomObject;
 import org.dcm4che2.data.Tag;
-import org.dcm4che2.data.VR;
 import org.dcm4che2.io.DicomInputStream;
+import org.dcm4che2.data.VR;
 import org.dcm4che2.io.DicomOutputStream;
 
 import java.io.File;
@@ -12,8 +11,8 @@ import java.io.FileOutputStream;
 public class dicomupdate {
 
     public static void main(String[] args) {
-        String dicomFilePath = "D:\\dcm4che2\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";  
-        String outputDicomPath = "D:\\dcm4che2\\update.dcm";
+        String dicomFilePath = "D:\\dicomreader\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";  
+        String outputDicomPath = "D:\\dicomreader\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";
 
         try {
             dicomupdate(dicomFilePath, outputDicomPath);
@@ -25,18 +24,17 @@ public class dicomupdate {
     private static void dicomupdate(String inputDicomPath, String outputDicomPath) throws Exception {
         File inputfile = new File(inputDicomPath);
         File outputfile = new File(outputDicomPath);
-
-        DicomInputStream dis = new DicomInputStream(new FileInputStream(inputfile));
-        DicomObject dcmObj = dis.readDicomObject();
-        dis.close();
-
+        
+        org.dcm4che3.io.DicomInputStream obj = new DicomInputStream(new FileInputStream(inputfile));
+        DicomObject dcmObj = obj.readDicomObject();
+        obj.close();
+        
         dcmObj.putString(Tag.PatientID, VR.LO, "101012345");
-        dcmObj.putString(Tag.PatientName, VR.PN, "lmn");
+        dcmObj.putString(Tag.PatientName, VR.PN, "XYZ");
 
-        DicomOutputStream dos = new DicomOutputStream(new FileOutputStream(outputfile));
-        dos.writeDicomFile(dcmObj); 
-        dos.close();
-
-        System.out.println(outputDicomPath);
+        org.dcm4che3.io.DicomOutputStream dos = new DicomOutputStream(new File(outputDicomPath));
+            dos.writeDicomFile(dcmObj.getFileMetaInformation(),dcmObj);
+            dos.close();
+        }
     }
 }

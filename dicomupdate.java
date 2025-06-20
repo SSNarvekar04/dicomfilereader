03d4c0f868068acf556db121b1f59398b824e22a
SSNarvekar04 <salonisachindranathnarvekar@gmail.com>
2025-06-20 15:22:38 +0530

    Update dicomupdate.java

--- dicomupdate.java
+++ dicomupdate.java
@@ -1,46 +1,42 @@
-import org.dcm4che3.data.Attributes;
-import org.dcm4che3.data.Tag;
-import org.dcm4che3.io.DicomInputStream;
-import org.dcm4che3.data.VR;
-import org.dcm4che3.io.DicomOutputStream;
-import org.dcm4che3.data.UID;
+
+import org.dcm4che2.data.DicomObject;
+import org.dcm4che2.data.Tag;
+import org.dcm4che2.data.VR;
+import org.dcm4che2.io.DicomInputStream;
+import org.dcm4che2.io.DicomOutputStream;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class dicomupdate {
 
     public static void main(String[] args) {
-        String dicomFilePath = "D:\\dicomreader\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";  
-        String outputDicomPath = "D:\\dicomreader\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";
+        String dicomFilePath = "D:\\dcm4che2\\1.2.40.0.13.1.183722267609197533024418606316316477527.dcm";  
+        String outputDicomPath = "D:\\dcm4che2\\update.dcm";
 
         try {
             dicomupdate(dicomFilePath, outputDicomPath);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void dicomupdate(String inputDicomPath, String outputDicomPath) throws IOException {
+    private static void dicomupdate(String inputDicomPath, String outputDicomPath) throws Exception {
         File inputfile = new File(inputDicomPath);
-        Attributes attrs;
-        Attributes meta;
+        File outputfile = new File(outputDicomPath);
 
-        try (DicomInputStream obj = new DicomInputStream(inputfile)) {
-            attrs = obj.readDataset(-1, -1);
-            meta = obj.readFileMetaInformation();
-        }
-        
-        attrs.setString(Tag.PatientID, VR.LO, "101012345");
-        attrs.setString(Tag.PatientName, VR.PN, "XYZ");
+        DicomInputStream dis = new DicomInputStream(new FileInputStream(inputfile));
+        DicomObject dcmObj = dis.readDicomObject();
+        dis.close();
 
-        if (meta == null) {
-            meta = attrs.createFileMetaInformation(UID.ImplicitVRLittleEndian);
-        }
+        dcmObj.putString(Tag.PatientID, VR.LO, "101012345");
+        dcmObj.putString(Tag.PatientName, VR.PN, "lmn");
 
-        try (DicomOutputStream dos = new DicomOutputStream(new File(outputDicomPath))) {
-            dos.writeFileMetaInformation(meta);
-            dos.writeDataset( meta,attrs);
-        }
+        DicomOutputStream dos = new DicomOutputStream(new FileOutputStream(outputfile));
+        dos.writeDicomFile(dcmObj); 
+        dos.close();
+
+        System.out.println(outputDicomPath);
     }
 }
